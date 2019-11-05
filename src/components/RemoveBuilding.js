5218e40346b8f735e8c5105efb8b13ca2cb9f8d7
caleb1000 <30327564+caleb1000@users.noreply.github.com>
2019-11-05 16:57:06 -0500

    Update RemoveBuilding.js

--- src/components/RemoveBuilding.js
+++ src/components/RemoveBuilding.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
 class RemoveBuilding extends React.Component {
+	
 	removeBuilding() {
+		
 		this.props.removeBuilding()
+	
 	}
 	render() {
 		return (
+			
 			<button onClick={this.removeBuilding.bind(this)}>
 				Remove Listing
-			</button>
+			//creating an on click button to let you remove building
+					</button>
 		);
 	}
 }
+
 
 export default RemoveBuilding;
